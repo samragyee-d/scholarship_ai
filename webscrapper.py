38d73885d39df56afe7a8133f168bd5c5af65aaf
Samragyee Dhakal <samragyeedhakal@gmail.com>
2024-12-20 11:36:55 -0500

    Prints only the 'Prompt: ' within <p>

--- webscrapper.py
+++ webscrapper.py
@@ -3,6 +3,8 @@
 #Importing libraries
 from bs4 import BeautifulSoup
 import requests
+import bs4
+import re
 
 #Obtained URL and defined the headers
 scrape_URL = "https://www.collegeessayguy.com/blog/scholarship-essay-examples#A"
@@ -14,5 +16,13 @@ HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 #The values in the HEADERS dictionary are sent with the HTTP GET request to the servers. The response (the website) is stored in the webpage variable 
 webpage = requests.get(scrape_URL, headers=HEADERS)
 
-#This extracts the html code from the information stored in the webpage variable. 
-soup = BeautifulSoup(webpage.content, 'html.parser')
+#This extracts the html code from the information stored in the webpage variable.
+
+soup = bs4.BeautifulSoup(webpage.content, 'html.parser')
+
+text = 'Prompt: '
+ 
+# Search by text with the help of lambda function
+gfg = soup.find_all(lambda tag: tag.name == "p" and text in tag.text)
+ 
+print(gfg)
